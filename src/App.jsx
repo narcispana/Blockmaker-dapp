0248f341f31b45598362690254fbae5841661ced
narcispana <narcisscoot10@gmail.com>
2024-04-02 12:24:28 +0200

    Añadir cositas

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
-import Header from "./Components/Header"
-import Footer from "./Components/Footer"
+import {Header, Footer} from './Components'
 
 function App() {
   return (
     <>
-      <Header/>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!</h1>
-      <Footer/>
+      <Header></Header>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Footer></Footer>
     </>
   )
 }
---
+++ src/Components/index.js
@@ -0,0 +1,2 @@
+export {Default as Footer} from './Footer';
+export {Default as Header} from './Header';
